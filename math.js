7fc75531dae857fb90e43315cbc7e75aa5d511cb
root <root@debian.debian>
2016-05-28 16:26:10 -0300

    math

---
+++ math.js
@@ -0,0 +1,12 @@
+(function (w){
+	var main = function () {
+		var d = document, b = d.body, div = d.createElement('div'),text = d.createTextNode('teste'),
+		signs=['+','-','*','/'];
+		div.appendChild(text);
+		b.appendChild(div);
+		var math = function(){
+			
+		};	
+	};
+	w.onload = main;
+})(window);
