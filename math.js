693f17427e4f93f9685221575b3d04efb77678a2
root <root@debian.debian>
2016-05-29 10:54:31 -1100

    number

--- math.js
+++ math.js
@@ -1,11 +1,11 @@
 (function (w){
 	var main = function () {
 		var d = document, b = d.body, div = d.createElement('div'),text = d.createTextNode('teste'),
-		signs=['+','-','*','/'];
+		signs=['+','-','*','/'],numbers=[0,1,2,3,4,5,6,7,8,9];
 		div.appendChild(text);
 		b.appendChild(div);
 		var math = function(){
-			
+			createDiv:function(att){},
 		};	
 	};
 	w.onload = main;
