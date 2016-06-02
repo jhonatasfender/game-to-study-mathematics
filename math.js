91f4f6e0f4fbf8d4c17dccf5453fb49188c4692c
Jonatas Rodrigues <jhonatas.fender@gmail.com>
2016-06-02 10:50:46 -1100

    Update math.js

--- math.js
+++ math.js
@@ -1,11 +1,11 @@
-(function (w){
-	var main = function () {
-		var d = document, b = d.body, div = d.createElement('div'),signs=['+','-','*','/'],numbers=[0,1,2,3,4,5,6,7,8,9],
-		signSelect = signs[Math.floor(Math.random()*3)], v1 = numbers[Math.floor(Math.random()*10)],
-		v2 = numbers[Math.floor(Math.random()*10)],calc = v1 + " " + signSelect + " " + v2,
-		text = d.createTextNode(calc);
-		div.appendChild(text);
-		b.appendChild(div);
-	};
-	w.onload = main;
+(function (w) {
+    var main = function () {
+        var d = document, b = d.body, div = d.createElement('div'), signs = ['+', '-', '*', '/'], numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+                signSelect = signs[Math.floor(Math.random() * 3)], v1 = numbers[Math.floor(Math.random() * 10)],
+                v2 = numbers[Math.floor(Math.random() * 10)], calc = v1 + " " + signSelect + " " + v2,
+                text = d.createTextNode(calc);
+        div.appendChild(text);
+        b.appendChild(div);
+    };
+    w.onload = main;
 })(window);
